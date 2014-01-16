55ceffbc93ddb2347398203be816067afd0885b3
cratein <alaric.calmette@gmail.com>
2014-01-16 12:22:50 +0100

    victor t'es une machine?

--- Sealed/src/Main.java
+++ Sealed/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 	//veux tu m'epouser?
 	//non t'es moche
 	public static void main(String[] args){
-		System.out.println("test");
+		System.out.println("victor t'es une machine?");
 	}
 }
